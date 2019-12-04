d373647abbd5ca60b982156c6749f7255efd2dde
jronceros80 <jronceros80@gmail.com>
2019-12-04 21:54:45 +0100

    ok

--- src/main/java/net/microservice/HystrixDashboard/Application.java
+++ src/main/java/net/microservice/HystrixDashboard/Application.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableHystrixDashboard
-@EnableTurbine
+//@EnableTurbine
 public class Application {
 
 	public static void main(String[] args) {
